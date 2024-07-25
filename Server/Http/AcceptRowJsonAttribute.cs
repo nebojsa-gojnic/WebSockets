fd40727069d450501e02a9be4663af9fe3d9df4a
nebojsa-gojnic <nebojsa.gojnic@cpu.rs>
2024-07-25 16:03:40 +0200

     

--- Server/Http/AcceptRowJsonAttribute.cs
+++ Server/Http/AcceptRowJsonAttribute.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Reflection ;
 
 namespace WebSockets 
-{
+{ 
 	/// <summary>
 	/// This attribute denotes that the method takes single JObject parameter.
 	/// </summary>
